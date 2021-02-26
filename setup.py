0c260d62b9a79484aab88fdad74a780216a5afd0
Calvin M.T <calvin.massonnet@gmail.com>
2021-02-26 19:48:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='tmsim',
+	version='1.0.0',
+	description='Turing Machine Simulator',
+	license='MIT',
+	author='Calvin M.T.',
+	url='https://calvinmt.com',
+	download_url='https://github.com/CalvinMT/TMSIM',
+	packages=find_packages()
+)
