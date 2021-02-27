48046d9ce94f3231377d6f5409437a62084a7f4b
Calvin M.T <calvin.massonnet@gmail.com>
2021-02-27 22:31:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='tmsim',
-	version='1.0.0',
+	version='1.1.0',
 	description='Turing Machine Simulator',
 	license='MIT',
 	author='Calvin M.T.',
